fcdcfc3367bd9513de0f890cc346e4cb9db0701f
Jeremy Johnson <jeremyjohnson@Jeremys-MacBook-Air.local>
2017-08-20 22:45:25 -0500

    added jquery and onclick funtions

---
+++ assets/javascript/logic.js
@@ -0,0 +1,68 @@
+// Global Variables
+// --------------------------------------------
+// Need to track value of crystals each round
+// Need to crack current and target score 
+// Need to track Win & Loss Count
+
+// Crystal Variables
+var crystal = {
+	donnie:
+	{
+		name: "donnie",
+		value: 0	
+	},
+	jordan:
+	{
+		name: "jordan",
+		value: 0	
+	},
+	margot:
+	{
+		name: "margot",
+		value: 0	
+	},
+	mark:
+	{
+		name: "mark",
+		value: 0	
+	},
+
+};
+
+
+//var donnie = 0;
+//var jordan = 0;
+//var margot = 0;
+//var mark = 0;
+
+// SCores (Current and Target)
+var currentScore = 0;
+var targetScore = 0;
+
+// Wins and Losses
+var winCount = 0;
+var lossCount = 0;
+
+
+// Functions
+// --------------------------------------------
+
+
+// Main Process
+// --------------------------------------------
+$("#donnie").click(function() {
+	alert("test");
+});
+
+$("#jordan").click(function() {
+	alert("test");
+});
+
+$("#margot").click(function() {
+	alert("test");
+});
+
+$("#mark").click(function() {
+	alert("test");
+});
+
